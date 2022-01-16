a2a8deeff64a4e1ffc8de368b30c4ee91faeb7ab
Houda saira <houdasaira@gmail.com>
2022-01-16 23:09:53 +0100

    changing in the enu

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -9,41 +9,41 @@ import alertTriangleFill from '@iconify/icons-eva/alert-triangle-fill';
 
 // ----------------------------------------------------------------------
 
-const getIcon = (name) => < Icon icon = { name }
-width = { 22 }
-height = { 22 }
+const getIcon = (name) => < Icon icon={name}
+    width={22}
+    height={22}
 />;
 
 const sidebarConfig = [{
-        title: 'dashboard',
-        path: '/dashboard/app',
-        icon: getIcon(pieChart2Fill)
-    },
-    {
-        title: 'students',
-        path: '/dashboard/user',
-        icon: getIcon(peopleFill)
-    },
-     {
-         title: 'timetables',
-         path: '/dashboard/products',
-         icon: getIcon(shoppingBagFill)
-     },
-    {
-        title: 'timetables',
-        path: '/dashboard/blog',
-        icon: getIcon(fileTextFill)
-    },
-    {
-        title: 'login',
-        path: '/login',
-        icon: getIcon(lockFill)
-    },
-    {
-        title: 'register',
-        path: '/register',
-        icon: getIcon(personAddFill)
-    },
+    title: 'dashboard',
+    path: '/dashboard/app',
+    icon: getIcon(pieChart2Fill)
+},
+{
+    title: 'students',
+    path: '/dashboard/user',
+    icon: getIcon(peopleFill)
+},
+// {
+//     title: 'timetables',
+//     path: '/dashboard/products',
+//     icon: getIcon(shoppingBagFill)
+// },
+// {
+//     title: 'timetables',
+//     path: '/dashboard/blog',
+//     icon: getIcon(fileTextFill)
+// },
+{
+    title: 'login',
+    path: '/login',
+    icon: getIcon(lockFill)
+},
+{
+    title: 'register',
+    path: '/register',
+    icon: getIcon(personAddFill)
+},
     // {
     //     title: 'Not found',
     //     path: '/404',
