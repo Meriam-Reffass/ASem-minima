480c609b556749b7f0978438e57a91080db5f393
Reffass Meriam <reffass.meriam@gmail.com>
2022-01-26 14:34:27 +0100

    fixing a bug that prohibits snackbar in login/register from showing

--- src/components/authentication/login/LoginForm.js
+++ src/components/authentication/login/LoginForm.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { useState } from 'react';
+import { useState, forwardRef } from 'react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useFormik, Form, FormikProvider } from 'formik';
 import { Icon } from '@iconify/react';
@@ -7,153 +7,132 @@ import eyeFill from '@iconify/icons-eva/eye-fill';
 import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
 // material
 import {
-    Link,
-    Stack,
-    Checkbox,
-    TextField,
-    IconButton,
-    InputAdornment,
-    FormControlLabel,
+  Link,
+  Stack,
+  Checkbox,
+  TextField,
+  IconButton,
+  InputAdornment,
+  FormControlLabel,
 } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 // ----------------------------------------------------------------------
 export default function LoginForm() {
-    const navigate = useNavigate();
-    const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
-    const LoginSchema = Yup.object().shape({
-        email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-        password: Yup.string().required('Password is required'),
-    });
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
 
-    const [open, setOpen] = useState(false);
-    const [error, setError] = useState('');
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState('');
 
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpen(false);
-    };
-
-
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            remember: true,
-        },
-        validationSchema: LoginSchema,
-        onSubmit: async(values) => {
-
-            const axios = require('axios');
-            axios
-                .post('http://localhost:3000/api/login', values)
-                .then((response) => {
-                    if (response.status == 200) {
-                        localStorage.setItem('token', response.data.data.token);
-                        localStorage.setItem('firstName', response.data.data.firstName);
-                        localStorage.setItem('lastName', response.data.data.lastName);
-                        navigate('/dashboard', { replace: true });
-                    } else {
-                        throw 'login failed';
-                    }
-                })
-                .catch((error) => {
-                    console.log(error.response.data.error);
-                    setError(error.response.data.error);
-                    setOpen(true);
-
-                });
-        },
-    });
-
-    const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
-
-    const handleShowPassword = () => {
-        setShowPassword((show) => !show);
-    };
-
-    return ( <
-        FormikProvider value = { formik } >
-        <
-        Form autoComplete = "off"
-        noValidate onSubmit = { handleSubmit } >
-        <
-        Stack spacing = { 3 } >
-        <
-        TextField fullWidth autoComplete = "username"
-        type = "email"
-        label = "Email address" {...getFieldProps('email') }
-        error = { Boolean(touched.email && errors.email) }
-        helperText = { touched.email && errors.email }
-        />{' '} <
-        TextField fullWidth autoComplete = "current-password"
-        type = { showPassword ? 'text' : 'password' }
-        label = "Password" {...getFieldProps('password') }
-        InputProps = {
-            {
-                endAdornment: ( <
-                    InputAdornment position = "end" >
-                    <
-                    IconButton onClick = { handleShowPassword }
-                    edge = "end" >
-                    <
-                    Icon icon = { showPassword ? eyeFill : eyeOffFill }
-                    />{' '} <
-                    /IconButton>{' '} <
-                    /InputAdornment>
-                ),
-            }
-        }
-        error = { Boolean(touched.password && errors.password) }
-        helperText = { touched.password && errors.password }
-        />{' '} <
-        /Stack>{' '} <
-        Stack direction = "row"
-        alignItems = "center"
-        justifyContent = "space-between"
-        sx = {
-            { my: 2 } } >
-        <
-        FormControlLabel control = { < Checkbox {...getFieldProps('remember') }
-            checked = { values.remember }
-            />}
-            label = "Remember me" /
-            >
-            <
-            Link component = { RouterLink }
-            variant = "subtitle2"
-            to = "#" >
-            Forgot password ?
-            <
-            /Link>{' '} <
-            /Stack>{' '} <
-            LoadingButton
-            fullWidth
-            size = "large"
-            type = "submit"
-            variant = "contained"
-            loading = { isSubmitting } >
-            Login { ' ' } <
-            /LoadingButton>{' '} <
-            /Form>{' '} <
-            Snackbar open = { open }
-            autoHideDuration = { 6000 }
-            onClose = { handleClose } >
-            <
-            MuiAlert onClose = { handleClose }
-            elevation = { 6 }
-            variant = "filled"
-            severity = "error"
-            sx = {
-                { width: '100%' } } > { error } <
-            /MuiAlert> <
-            /Snackbar> <
-            /FormikProvider>
-        );
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
     }
+
+    setOpen(false);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      remember: true,
+    },
+    validationSchema: LoginSchema,
+    onSubmit: async (values) => {
+      const axios = require('axios');
+      axios
+        .post('http://localhost:3000/api/login', values)
+        .then((response) => {
+          if (response.status == 200) {
+            localStorage.setItem('token', response.data.data.token);
+            localStorage.setItem('firstName', response.data.data.firstName);
+            localStorage.setItem('lastName', response.data.data.lastName);
+            navigate('/dashboard', { replace: true });
+          } else {
+            throw 'login failed';
+          }
+        })
+        .catch((error) => {
+          console.log(error.response.data.error);
+          setError(error.response.data.error);
+          setOpen(true);
+        });
+    },
+  });
+
+  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
+
+  const handleShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email address"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleShowPassword} edge="end">
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+        </Stack>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+          <FormControlLabel
+            control={<Checkbox {...getFieldProps('remember')} checked={values.remember} />}
+            label="Remember me"
+          />
+          <Link component={RouterLink} variant="subtitle2" to="#">
+            Forgot password ?
+          </Link>
+        </Stack>
+        <LoadingButton
+          fullWidth
+          size="large"
+          type="submit"
+          variant="contained"
+          loading={isSubmitting}>
+          Login
+        </LoadingButton>
+      </Form>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {error}
+        </Alert>
+      </Snackbar>
+    </FormikProvider>
+  );
+}
