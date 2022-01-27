1ad5d4f225cf8eff3eab18e3bfc318b89ab0b01f
outcast292 <reffass.meriam@gmail.com>
2022-01-27 20:45:15 +0100

    adding possibility to check students table

--- src/pages/User.js
+++ src/pages/User.js
@@ -26,17 +26,18 @@ import Label from '../components/Label';
 import Scrollbar from '../components/Scrollbar';
 import SearchNotFound from '../components/SearchNotFound';
 import { UserListHead, UserListToolbar, UserMoreMenu } from '../components/_dashboard/user';
+const axios = require("axios");
 //
 import USERLIST from '../_mocks_/user';
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Name', alignRight: false },
-  { id: 'company', label: 'Speciality', alignRight: false },
-  { id: 'role', label: 'In Hours', alignRight: false },
+  { id: 'firstName', label: 'firstName', alignRight: false },
+  { id: 'lastName', label: 'Lastname', alignRight: false },
+  { id: 'className', label: 'Speciality', alignRight: false },
+  { id: 'role', label: 'Abs N.justified ', alignRight: false },
   { id: 'isVerified', label: 'Level', alignRight: false },
-  { id: 'status', label: 'Status', alignRight: false },
   { id: '' },
 ];
 
@@ -66,7 +67,7 @@ function applySortFilter(array, comparator, query) {
     return a[1] - b[1];
   });
   if (query) {
-    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+    return filter(array, (_user) => _user.lastName.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   }
   return stabilizedThis.map((el) => el[0]);
 }
@@ -75,9 +76,21 @@ export default function User() {
   const [page, setPage] = useState(0);
   const [order, setOrder] = useState('asc');
   const [selected, setSelected] = useState([]);
-  const [orderBy, setOrderBy] = useState('name');
+  const [users, setUsers] = useState([]);
+  const [orderBy, setOrderBy] = useState('firstName');
   const [filterName, setFilterName] = useState('');
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  if (users.length == 0) {
+    axios.get("http://localhost:3000/api/students", { headers: { "auth-token": localStorage.token } }).then(resp => {
+      setUsers(resp.data)
+    }).
+      catch(err => {
+        console.log(err)
+      })
+  }
+
+
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -87,18 +100,18 @@ export default function User() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelecteds = USERLIST.map((n) => n.name);
+      const newSelecteds = users.map((n) => n._id);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, id) => {
+    const selectedIndex = selected.indexOf(id);
     let newSelected = [];
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, id);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -125,9 +138,9 @@ export default function User() {
     setFilterName(event.target.value);
   };
 
-  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - USERLIST.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - users.length) : 0;
 
-  const filteredUsers = applySortFilter(USERLIST, getComparator(order, orderBy), filterName);
+  const filteredUsers = applySortFilter(users, getComparator(order, orderBy), filterName);
 
   const isUserNotFound = filteredUsers.length === 0;
 
@@ -136,15 +149,15 @@ export default function User() {
       <Container>
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
-            Student{' '}
-          </Typography>{' '}
+            Student
+          </Typography>
           <Button
             variant="contained"
             component={RouterLink}
             to="#"
             startIcon={<Icon icon={plusFill} />}>
-            New Student{' '}
-          </Button>{' '}
+            New Student
+          </Button>
         </Stack>
 
         <Card>
@@ -165,19 +178,19 @@ export default function User() {
                   numSelected={selected.length}
                   onRequestSort={handleRequestSort}
                   onSelectAllClick={handleSelectAllClick}
-                />{' '}
+                />
                 <TableBody>
-                  {' '}
+
                   {filteredUsers
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map((row) => {
-                      const { id, name, role, status, company, avatarUrl, isVerified } = row;
-                      const isItemSelected = selected.indexOf(name) !== -1;
+                      const { _id, firstName, lastName, studyClass, nonVerifiedHours } = row;
+                      const isItemSelected = selected.indexOf(_id) !== -1;
 
                       return (
                         <TableRow
                           hover
-                          key={id}
+                          key={_id}
                           tabIndex={-1}
                           role="checkbox"
                           selected={isItemSelected}
@@ -185,56 +198,56 @@ export default function User() {
                           <TableCell padding="checkbox">
                             <Checkbox
                               checked={isItemSelected}
-                              onChange={(event) => handleClick(event, name)}
-                            />{' '}
+                              onChange={(event) => handleClick(event, _id)}
+                            />
                           </TableCell>
                           <TableCell component="th" scope="row" padding="none">
                             <Stack direction="row" alignItems="center" spacing={2}>
-                              <Avatar alt={name} src={avatarUrl} />{' '}
+                              <Avatar alt={name} />
                               <Typography variant="subtitle2" noWrap>
-                                {' '}
-                                {name}{' '}
-                              </Typography>{' '}
-                            </Stack>{' '}
-                          </TableCell>{' '}
-                          <TableCell align="left"> {role} </TableCell>{' '}
-                          <TableCell align="left"> {company} </TableCell>{' '}
-                          <TableCell align="left"> {isVerified } </TableCell>{' '}
+
+                                {firstName}
+                              </Typography>
+                            </Stack>
+                          </TableCell>
+                          <TableCell align="left"> {lastName} </TableCell>
+                          <TableCell align="left"> {studyClass.className} </TableCell>
                           <TableCell align="left">
                             <Label
                               variant="ghost"
-                              color={(status === 'Exclusion'  && 'error') || (status === 'Blame'  && 'warning') || (status === 'Warning'  && 'info') || 'success'}>
-                              {sentenceCase(status)}{' '}
-                            </Label>{' '}
+                              color={(nonVerifiedHours > 30 && 'error') || (nonVerifiedHours > 20 && 'warning') || (nonVerifiedHours > 10 && 'info') || 'success'}>
+                              {nonVerifiedHours}
+                            </Label>
                           </TableCell>
+                          <TableCell align="left"> {studyClass.promo} </TableCell>
                           <TableCell align="right">
                             <UserMoreMenu />
-                          </TableCell>{' '}
+                          </TableCell>
                         </TableRow>
                       );
-                    })}{' '}
+                    })}
                   {emptyRows > 0 && (
                     <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={6} />{' '}
+                      <TableCell colSpan={6} />
                     </TableRow>
-                  )}{' '}
-                </TableBody>{' '}
+                  )}
+                </TableBody>
                 {isUserNotFound && (
                   <TableBody>
                     <TableRow>
                       <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                        <SearchNotFound searchQuery={filterName} />{' '}
-                      </TableCell>{' '}
-                    </TableRow>{' '}
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
                   </TableBody>
-                )}{' '}
+                )}
               </Table>
             </TableContainer>
           </Scrollbar>
           <TablePagination
             rowsPerPageOptions={[5, 10, 25]}
             component="div"
-            count={USERLIST.length}
+            count={users.length}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
