ee3564563f35ddb2d75adf9ec1003a5a3446adb3
outcast292 <reffass.meriam@gmail.com>
2022-01-28 10:17:11 +0100

    adding modal to show form

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,7 @@
 import { useFormik } from 'formik';
 import { useState } from 'react';
 // material
-import { Container, Stack, Typography ,Button} from '@mui/material';
+import { Container, Stack, Typography, Button } from '@mui/material';
 // components
 import Page from '../components/Page';
 import {
@@ -16,12 +16,25 @@ import plusFill from '@iconify/icons-eva/plus-fill';
 import Timetables from "../components/timetables"
 //
 import PRODUCTS from '../_mocks_/products';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 // ----------------------------------------------------------------------
 
 export default function EcommerceShop() {
   const [openFilter, setOpenFilter] = useState(false);
 
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   const formik = useFormik({
     initialValues: {
       gender: '',
@@ -49,20 +62,37 @@ export default function EcommerceShop() {
     handleSubmit();
     resetForm();
   };
-
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Page title="Dashboard: Products | Minimal-UI">
       <Container>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
             Timetables
           </Typography>
           <Button
             variant="contained"
-            to="#"
+            onClick={handleOpen}
             startIcon={<Icon icon={plusFill} />}>
             New Class
           </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </Stack>
 
         {/* <Stack
